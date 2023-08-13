6f0dc9a18bc6cbbc57200edcb2b6e75fd3e928e9
Mueem-Nahid <mueem51@gmail.com>
2023-08-13 19:41:13 +0600

    # Feature - add footer and fixes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,57 @@
+import {ActionIcon, Center, Container, createStyles, Group, rem, Text} from '@mantine/core';
+import {IconBrandInstagram, IconBrandTwitter, IconBrandYoutube} from '@tabler/icons-react';
+import PCAlchemistLogo from "@/assets/pc-alchemist-logo.png";
+import Image from "next/image";
+
+const useStyles = createStyles((theme) => ({
+   footer: {
+      marginTop: rem(120),
+      borderTop: `${rem(1)} solid ${
+         theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+      }`,
+   },
+
+   inner: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingTop: theme.spacing.xl,
+      paddingBottom: theme.spacing.xl,
+
+      [theme.fn.smallerThan('xs')]: {
+         flexDirection: 'column',
+      },
+   },
+
+   links: {
+      [theme.fn.smallerThan('xs')]: {
+         marginTop: theme.spacing.md,
+      },
+   },
+}));
+
+export function Footer() {
+   const {classes} = useStyles();
+
+   return (
+      <div className={classes.footer}>
+         <Container className={classes.inner}>
+            <Image src={PCAlchemistLogo} height={60} width={90} alt="pc-alchemist"/>
+            <Center>
+               <Text color='gray'>mueem51@gmail.com</Text>
+            </Center>
+            <Group spacing={0} className={classes.links} position="right" noWrap>
+               <ActionIcon size="lg">
+                  <IconBrandTwitter size="1.05rem" stroke={1.5}/>
+               </ActionIcon>
+               <ActionIcon size="lg">
+                  <IconBrandYoutube size="1.05rem" stroke={1.5}/>
+               </ActionIcon>
+               <ActionIcon size="lg">
+                  <IconBrandInstagram size="1.05rem" stroke={1.5}/>
+               </ActionIcon>
+            </Group>
+         </Container>
+      </div>
+   );
+}
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,13 @@
 import {ReactNode} from "react";
 import {Navbar} from "@/components/Navbar";
+import {Footer} from "@/components/Footer";
 
 function RootLayout({children}: { children: ReactNode }) {
    return (
       <>
          <Navbar/>
          <main>{children}</main>
-         {/*<Footer/>*/}
+         <Footer/>
       </>
    )
 }
